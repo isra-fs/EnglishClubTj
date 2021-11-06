01f7f60f0845121763c2cb95b08b18422e48e682
Israel Fonseca <isra.fsol@gmail.com>
2021-11-06 10:57:47 -0700

    Added Video on clases

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,33 +1,31 @@
 
 export const Services = (props) => {
   return (
-    <div id='services' className='text-center'>
-      <div className='container'>
-        <div className='section-title'>
-          <h2>{props.data.title}</h2>
-          <p>
-           {props.data.description}
-          </p>
-        </div>
-        <div className='row services-info'>
-          {props.data
-            ? props.data.services.map((d, i) => (
-                <div key={`${d.name}-${i}`} className='col-sm-3'>
-                  {' '}
-                  <i ><img src= {"../img/services/" + d.icon}style={{width:'100px'}} /></i>
-                  <div className='service-desc'>
-                    <h3>{d.name}</h3>
-                    <p>{d.text}</p>
-                  </div>
-                </div>
-              ))
-            : 'loading'}
-        </div>
-		
-		
-      </div>
-	  <div style={{width:"100%"}}>
-		<img src="../img/services/services.jpg"   style={{width:"100%"}} alt="" />
+    	<div id='services' className='text-center'>
+			<div className='container'>
+				<div className='section-title'>
+					<h2>{props.data.title}</h2>
+					<p>
+						{props.data.description}
+					</p>
+				</div>
+				<div className='row services-info'>
+				{props.data
+					? props.data.services.map((d, i) => (
+						<div key={`${d.name}-${i}`} className='col-sm-3'>
+						{' '}
+						<i ><img src= {"../img/services/" + d.icon}style={{width:'100px'}} /></i>
+						<div className='service-desc'>
+							<h3>{d.name}</h3>
+							<p>{d.text}</p>
+						</div>
+						</div>
+					))
+					: 'loading'}
+				</div>
+      		</div>
+	  	<div style={{width:"100%"}}>
+		  <img src="../img/services/services.jpg"   style={{width:"100%"}} alt="" />
 		</div>
     </div>
   )
