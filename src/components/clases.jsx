97718277c14e1f15a31ed76963bd2dc5474f74ac
Israel Fonseca <isra.fsol@gmail.com>
2021-12-04 16:03:08 -0800

    Update design in clases

--- src/components/clases.jsx
+++ src/components/clases.jsx
@@ -3,16 +3,31 @@ export const Clases = (props) => {
     return (
       <div id='clases' className='text-center'>
         <div className='container'>
+
           <div className='section-title'>
             <h2>{props.data.title}</h2>
             <p>
              {props.data.description}
             </p>
           </div>
+		  <div className='row'>
+            {props.data
+              ? props.data.clases.slice(0,3).map((d, i) => (
+                  <div key={`${d.name}-${i}`} className='col-6 col-md-4'>
+                    {' '}
+                    <i ><img src= {"img/clases/" + d.icon}style={{width:'100px'}} /></i>
+                    <div className='service-desc'>
+                      <h3>{d.name}</h3>
+                      <p>{d.text}</p>
+                    </div>
+                  </div>
+                ))
+              : 'loading'}
+          </div>
           <div className='row'>
             {props.data
-              ? props.data.clases.map((d, i) => (
-                  <div key={`${d.name}-${i}`} className='col-sm-3'>
+              ? props.data.clases.slice(3).map((d, i) => (
+                  <div key={`${d.name}-${i}`} className='col-6 col-md-4'>
                     {' '}
                     <i ><img src= {"img/clases/" + d.icon}style={{width:'100px'}} /></i>
                     <div className='service-desc'>
